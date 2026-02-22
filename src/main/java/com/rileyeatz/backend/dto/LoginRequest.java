53294e2defcf7d0fd899fbb0cb233aef5446eb0a
Girish K Bill <gir****@gmail.com>
2026-02-22 23:25:33 +0530

    Fix mvnw permission for Render

--- src/main/java/com/rileyeatz/backend/dto/LoginRequest.java
+++ src/main/java/com/rileyeatz/backend/dto/LoginRequest.java
@@ -1,31 +1,31 @@
-package com.rileyeatz.backend.dto;
+    package com.rileyeatz.backend.dto;
 
-public class LoginRequest {
+    public class LoginRequest {
 
-    private String email;
-    private String password;
+        private String email;
+        private String password;
 
-    public LoginRequest() {
+        public LoginRequest() {
+        }
+
+        public LoginRequest(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
     }
-
-    public LoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
