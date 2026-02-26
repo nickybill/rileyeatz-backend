afa05f8ea60959de02da230ecb267880c1ffb12e
Girish K Bill <gir****@gmail.com>
2026-02-26 22:27:28 +0530

    Fix mvnw permission for Render

--- src/main/java/com/rileyeatz/backend/repository/AdminRepository.java
+++ src/main/java/com/rileyeatz/backend/repository/AdminRepository.java
@@ -2,8 +2,13 @@ package com.rileyeatz.backend.repository;
 
 import com.rileyeatz.backend.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Optional<Admin> findByEmail(String email);
+
+    @Query("SELECT a FROM Admin a LEFT JOIN FETCH a.restaurant WHERE a.email = :email")
+    Optional<Admin> findByEmailWithRestaurant(@Param("email") String email);
 }
