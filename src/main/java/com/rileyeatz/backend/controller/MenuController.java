209bf361b7a5eabb7e75ac32864e6b06010336a2
Girish K Bill <gir****@gmail.com>
2026-02-26 05:45:04 +0530

    Fix mvnw permission for Render

--- src/main/java/com/rileyeatz/backend/controller/MenuController.java
+++ src/main/java/com/rileyeatz/backend/controller/MenuController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import com.rileyeatz.backend.model.Restaurant;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
